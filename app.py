aca19a36b0944cf6012959f12f3dbf9324044c8a
Doan Hoang <156933418+doan21@users.noreply.github.com>
2026-05-04 22:08:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def chat():
     return {'reply': data['choices'][0]['message']['content']}
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=5000)
